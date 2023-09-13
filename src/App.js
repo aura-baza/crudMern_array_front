062a752bcba4cdbdd76d8dcbcbeb4d959d2d048e
aura-baza <aurabaza17@gmail.com>
2023-09-13 18:00:26 -0500

    iniciando

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React con pacho
         </a>
       </header>
     </div>
